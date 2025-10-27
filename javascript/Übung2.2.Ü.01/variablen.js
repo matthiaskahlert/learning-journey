bc3177fe6d606e6ee4d8c8a11eb8cd6587ce3ca1
Matthias Kahlert <m-kahlert@web.de>
2025-10-27 17:02:44 +0100

    javascript-learning - changed file location for my notes and exercises

--- javascript/Übung2.2.Ü.01/variablen.js
+++
@@ -1,7 +0,0 @@
-// Variablen definieren
-let alter = 45;
-const benutzerName = "Matthias";
-
-// Nachricht in der Browserconsole ausgeben
-console.log("Hallo, mein Name ist:", benutzerName, "und ich bin", alter, "Jahre alt.");
-

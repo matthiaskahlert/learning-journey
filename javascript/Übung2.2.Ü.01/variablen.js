61a379ae7cb540704f76c4752cf1b8f2b1c75949
Matthias Kahlert <65134174+matthiaskahlert@users.noreply.github.com>
2025-10-27 14:10:28 +0100

    small excercise added
    Erstelle eine einfache HTML-Seite mit dem Titel "Meine erste JavaScript-Seite" und binde eine externe JavaScript-Datei ein. In der JavaScript-Datei definiere zwei Variablen: eine mit dem Namen benutzerName, die deinen Namen als Zeichenkette speichert, und eine zweite Variable alter, die dein Alter als Zahl speichert. Schreibe dann eine Anweisung, die eine Nachricht im Konsolenfenster ausgibt, welche deinen Namen und dein Alter in einem vollständigen Satz zusammenfasst. Die Nachricht sollte lauten: "Hallo, mein Name ist [dein Name] und ich bin [dein Alter] Jahre alt.

---
+++ javascript/Übung2.2.Ü.01/variablen.js
@@ -0,0 +1,7 @@
+// Variablen definieren
+let alter = 45;
+const benutzerName = "Matthias";
+
+// Nachricht in der Browserconsole ausgeben
+console.log("Hallo, mein Name ist:", benutzerName, "und ich bin", alter, "Jahre alt.");
+

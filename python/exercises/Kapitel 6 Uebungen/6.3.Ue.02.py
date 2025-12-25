a547558b20f43c3bb9b9e9defcf007102b3242d6
Matthias Kahlert <m-kahlert@web.de>
2025-12-25 01:44:43 +0100

    py exercise unfinished

---
+++ python/exercises/Kapitel 6 Uebungen/6.3.Ue.02.py
@@ -0,0 +1,11 @@
+""" 
+a) Importiere das Modul random und verwende eine Funktion aus diesem Modul, um eine Zufallszahl zwischen 1 und 10 zu generieren. 
+Speichere diese Zahl in einer Variablen.
+
+b) Definiere eine Funktion namens berechne_quadrat, die einen Parameter nimmt, dessen Quadrat berechnet und das Ergebnis zurückgibt.
+
+c) Verwende eine if-Kontrollstruktur, um zu überprüfen, ob die generierte Zufallszahl größer als 5 ist. 
+Falls ja, rufe die Funktion berechne_quadrat mit der Zufallszahl als Argument auf und gib das Ergebnis aus. 
+Falls nein, gib eine Nachricht aus, die besagt, dass die Zahl kleiner oder gleich 5 ist.
+
+d) Implementiere eine Schleife, die von 1 bis zur generierten Zufallszahl läuft und dabei jedes Mal die aktuelle Zahl ausgibt.  """

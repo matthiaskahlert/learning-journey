978074317b15f93991c2ed73320814e56b389e8f
Matthias Kahlert <m-kahlert@posteo.de>
2025-12-25 23:27:06 +0100

    python Übung 6.3.Ue.02 random randint, range

--- python/exercises/Kapitel 6 Uebungen/6.3.Ue.02.py
+++ python/exercises/Kapitel 6 Uebungen/6.3.Ue.02.py
@@ -9,3 +9,25 @@ Falls ja, rufe die Funktion berechne_quadrat mit der Zufallszahl als Argument au
 Falls nein, gib eine Nachricht aus, die besagt, dass die Zahl kleiner oder gleich 5 ist.
 
 d) Implementiere eine Schleife, die von 1 bis zur generierten Zufallszahl läuft und dabei jedes Mal die aktuelle Zahl ausgibt.  """
+
+import random
+from math import sqrt
+# a) 
+zufallszahl = random.randint(1, 10) # Return random integer in range [a, b], including both end points.
+
+# b)
+def berechne_quadrat(x):
+    ergebnis1 = sqrt(x)
+    return ergebnis1
+
+# c)
+if zufallszahl >5:
+    ergebnis2 = berechne_quadrat(zufallszahl)
+    print(f"{zufallszahl} ist größer als 5. ")
+    print(f"Die Quafratwurzel von {zufallszahl} ist {ergebnis2:.2f}")
+else:
+    print(f"{zufallszahl} ist kleiner als 5. ")
+
+# d)
+for i in range(1, zufallszahl + 1): # start ist inklusive stopwert ist eklusive, daher + 1
+    print(i)
